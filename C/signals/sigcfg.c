de45d6080f99963ae72522bb4dcc4b25b45d44f9
netoe1 <elynetobr@outlook.com>
2024-10-22 17:20:04 -0300

    unactive sigcfg.c

--- C/signals/sigcfg.c
+++ C/signals/sigcfg.c
@@ -1,25 +1,26 @@
-#ifndef SIGCFG_C
-#define SIGCFG_C
-#include <stdlib.h>
+// #ifndef SIGCFG_C
+// #define SIGCFG_C
+// #include <stdlib.h>
 
-void handle_signal(int signal, char ptr)
-{
-    printf("Sinal %d capturado.\n", signal);
+// void handle_signal(int signal)
+// {
+//     printf("Sinal %d capturado.\n", signal);
 
-    // Limpa o ponteiro se ele não for NULL
-    if (ptr != NULL)
-    {
-        printf("Limpando ponteiro...\n");
-        free(ptr);
-        ptr = NULL;
-        printf("Ponteiro limpo com sucesso!\n");
-    }
-    else
-    {
-        printf("Ponteiro já está limpo.\n");
-    }
+//     // Limpa o ponteiro se ele não for NULL
+//     if (ptr != NULL)
+//     {
+//         printf("Cleaning ptr\n");
+//         free(ptr);
+//         ptr = NULL;
+//         printf("Ponteiro limpo com sucesso!\n");
+//     }
+//     else
+//     {
+//         printf("Ponteiro já está limpo.\n");
+//         return;
+//     }
 
-    // Pode-se decidir terminar o programa após o tratamento do sinal
-    exit(0);
-}
-#endif
+//     // Pode-se decidir terminar o programa após o tratamento do sinal
+//     exit(0);
+// }
+// #endif
